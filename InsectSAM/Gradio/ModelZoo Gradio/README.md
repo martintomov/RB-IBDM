182fb2fec404cba0527b47a7f684ef04091d7754
martintmv <work.martintmv@gmail.com>
2024-06-11 14:20:14 +0200

    modelzoo update

---
+++ InsectSAM/Gradio/ModelZoo Gradio/README.md
@@ -0,0 +1,12 @@
+---
+title: Model Zoo
+emoji: 🐞
+colorFrom: blue
+colorTo: green
+sdk: gradio
+sdk_version: 4.36.1
+app_file: app.py
+pinned: true
+license: apache-2.0
+short_description: Comparing InsectSAM, Yolov8, Detectron2
+---
--- InsectSAM/Gradio/ModelZoo Gradio/app.py
+++ InsectSAM/Gradio/ModelZoo Gradio/app.py
@@ -1,10 +1,16 @@
+try:
+    import detectron2
+except:
+    import os 
+    os.system('pip install git+https://github.com/facebookresearch/detectron2.git')
+
 import gradio as gr
 import json
 import numpy as np
 from sam_utils import grounded_segmentation, create_yellow_background_with_insects
 from yolo_utils import yolo_processimage
 from detectron_utils import detectron_process_image
-def process_image(image):
+def process_image(image, include_json):
     detectron_result=detectron_process_image(image)
     yolo_result = yolo_processimage(image)
     insectsam_result = create_yellow_background_with_insects(image)
@@ -13,16 +19,15 @@ def process_image(image):
 
 examples = [
     ["demo.jpg"],
-    ["demo1.jpg"],
-    ["demo2.jpg"]
+    ["demo-2.jpg"],
 ]
 
 gr.Interface(
     fn=process_image,
-    inputs=[gr.Image(type="pil")],
+    inputs=[gr.Image(type="pil"), gr.Checkbox(label="Include JSON", value=False)],
     outputs=[gr.Image(label='InsectSAM', type="numpy"),
              gr.Image(label='Yolov8', type="numpy"),
              gr.Image(label='Detectron', type="numpy")],
-    title="Model Zoo 🐞",
+    title="Model Zoo🐞",
     examples=examples
 ).launch()
